b0110284afb141e0cc73aa4f27d03b9ebb12949b
DZ BAM Server <dz-bam@richard-neumann.de>
2023-01-11 17:34:20 +0100

    Create eAI_init.c

---
+++ eAI/eAI_init.c
@@ -0,0 +1,13 @@
+ref array<Patrol> PATROLS = {
+	new Patrol({"3161.399170 22.615067 4068.234863", "3125.670166 22.462461 4063.274414"}),
+};
+
+autoptr array<autoptr eAIDynamicPatrol> EAI_DYNAMIC_PATROLS = {};
+
+void InitDynamicPatrols() {
+	for (int i = 0; i < PATROLS.Count(); i++) {
+		autoptr eAIDynamicPatrol patrol = PATROLS[i].getDynamicPatrol();
+		EAI_DYNAMIC_PATROLS.Insert(patrol);
+		patrol.UpdateTriggers();
+	}
+}
