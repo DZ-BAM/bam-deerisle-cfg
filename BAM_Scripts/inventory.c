3eb31fc5b93bb395c42109498212838bfc686fc9
Richard Neumann <mail@richard-neumann.de>
2023-06-23 21:33:27 +0200

    Fix reference errors

--- BAM_Scripts/inventory.c
+++ BAM_Scripts/inventory.c
@@ -1,18 +1,15 @@
 #ifndef BAM_INVENTORY
 #define BAM_INVENTORY
 
-typedef array<array<string>> BAM_ItemSelections;
-typedef array<BAM_QuantizedItem> BAM_QuantizedItems;
-
 class BAM_Inventory {
     array<string> items;
-    BAM_ItemSelections random_items;
-    BAM_QuantizedItems quantized_items;
+    array<array<string>> random_items;
+    map<string, int> quantized_items;
 
     void BAM_Inventory(
             array<string> items,
-            BAM_ItemSelections random_items,
-            BAM_QuantizedItems quantized_items,
+            array<array<string>> random_items,
+            map<string, int> quantized_items,
     ) {
         this.items = items;
         this.random_items = random_items;
@@ -24,41 +21,28 @@ class BAM_Inventory {
     }
 
     void addToInventory(GameInventory inventory) {
-        if (this.items) {
-            foreach (auto item: this.items) {
-                inventory.CreateInInventory(item);
-            }
+
+        foreach (auto item: this.items) {
+            inventory.CreateInInventory(item);
         }
 
-        if (this.random_items) {
-            foreach (auto selection: this.random_items) {
-                inventory.CreateInInventory(selection.GetRandomElement());
-            }
+        foreach (auto selection: this.random_items) {
+            inventory.CreateInInventory(selection.GetRandomElement());
         }
 
-        if (this.quantized_items) {
-            foreach (auto quantized_item: this.quantized_items) {
-                quantized_item.addToInventory(inventory);
-            }
-        }
-    }
-};
-
-class BAM_QuantizedItem {
-    string name;
-    int quantity;
-
-    void BAM_QuantizedItem(string name, int quantity) {
-        this.name = name;
-        this.quantity = quantity;
+        this.addQuantizedItemsToInventory(inventory);
     }
 
-    void addToInventory(GameInventory inventory) {
-        EntityAI entity = inventory.CreateInInventory(this.name);
+    void addQuantizedItemsToInventory(GameInventory inventory) {
+        EntityAI entity;
         ItemBase item;        
         
-        if (Class.CastTo(item, entity)) {
-            item.SetQuantity(this.quantity);
+        foreach (auto name, auto quantity: this.quantized_items) {
+            entity = inventory.CreateInInventory(name);
+
+            if (Class.CastTo(item, entity)) {
+                item.SetQuantity(quantity);
+            }
         }
     }
 };
--- BAM_Scripts/spawn.c
+++ BAM_Scripts/spawn.c
@@ -1,28 +1,33 @@
 #ifndef BAM_SPAWN
 #define BAM_SPAWN
+
 #include "$CurrentDir:bam-deerisle-cfg/BAM_Scripts/clothing.c"
 #include "$CurrentDir:bam-deerisle-cfg/BAM_Scripts/inventory.c"
 
-const autoptr BAM_Clothing CLOTHING = new BAM_Clothing(
-	"ChernarusSportShirt",
-	"SlacksPants_Beige",
-	"jmc_Military_mountain_boots_black",
-	"",
-	"",
-	"",
-);
-const autoptr BAM_Inventory INVENTORY = new BAM_Inventory(
-	{"CombatKnife", "Rag"},
-	{
-		{"Chemlight_White", "Chemlight_Yellow", "Chemlight_Green", "Chemlight_Red"},
-		{"Apple", "Pear", "Plum"},
-	},
-	{
-		new BAM_QuantizedItem("Rag", 4),
-	},
-);
-
 void BAM_SetStartingEquipment(PlayerBase player) {
+	auto QUANTIZED_ITEMS = new map<string, int>();
+	QUANTIZED_ITEMS["Rags"] = 4;
+
+	auto CLOTHING = new BAM_Clothing(
+		"ChernarusSportShirt",
+		"SlacksPants_Beige",
+		"jmc_Military_mountain_boots_black",
+		"",
+		"",
+		"",
+	);
+
+	array<string> ITEMS = {"CombatKnife"};
+	array<string> CHEMLIGHTS = {
+		"Chemlight_White",
+		"Chemlight_Yellow",
+		"Chemlight_Green",
+		"Chemlight_Red",
+	};
+	array<string> FRUITS = {"Apple", "Pear", "Plum"};
+	array<array<string>> RANDOM_ITEMS = {CHEMLIGHTS, FRUITS};
+	auto INVENTORY = new BAM_Inventory(ITEMS, RANDOM_ITEMS, QUANTIZED_ITEMS);
+
 	CLOTHING.applyTo(player);
 	INVENTORY.addToPlayer(player);
 }
