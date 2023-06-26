805d537e4ffb5f2839b78d3596827cf433878345
Richard Neumann <mail@richard-neumann.de>
2023-06-26 17:38:41 +0200

    Move scripts to separate repo

--- BAM_Scripts/error.c
+++
@@ -1,8 +0,0 @@
-#ifndef BAM_ERROR
-#define BAM_ERROR
-
-void BAM_error(string message) {
-    Print("BAM Error: " + message);
-}
-
-#endif
--- BAM_Scripts/main.c
+++
@@ -1,8 +0,0 @@
-#ifndef BAM_MAIN
-#define BAM_MAIN
-
-#ifndef BAM_SPAWN
-#include "$CurrentDir:bam-deerisle-cfg/BAM_Scripts/spawn.c"
-#endif
-
-#endif
